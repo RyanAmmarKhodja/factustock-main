6dcc2e067ced70e609e17240debb73b0c7fe3f44
RyanAmmarKhodja <ammarkhodjarayane2003@gmail.com>
2026-01-31 21:13:07 +0100

    Initial

---
+++ backend/faturaflow/faturaflow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace faturaflow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
